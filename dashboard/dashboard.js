da54519bc0a09e1fc05fba55cdd4ef13728176c6
JDxSen <jd.x.senators@gmail.com>
2024-08-25 21:51:04 +0530

    Initial commit

---
+++ dashboard/dashboard.js
@@ -0,0 +1,151 @@
+import express from 'express'
+import session from 'express-session'
+import minify from 'express-minify'
+import ejs from 'ejs'
+import memorystore from 'memorystore'
+const MemoryStore = memorystore(session)
+import { Routes } from 'discord-api-types/v10'
+
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { randomBytes } from 'crypto'
+import { marked } from 'marked'
+import { setupWebsocket } from './websocket.js'
+import { adminId, appId, clientSecret } from '../utilities/config.js'
+import { logging } from '../utilities/logging.js'
+
+const app = express()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export function startDashboard(client) {
+  const port = 80
+  const domain = 'https://suitbot.xyz'
+  const host = process.env.NODE_ENV === 'production' ? domain : 'http://localhost'
+
+  // Set EJS engine
+  app.engine('ejs', ejs.renderFile)
+  app.set('view engine', 'ejs')
+
+  // Minify and set files
+  app.use(minify())
+  app.use('/assets', express.static(path.join(__dirname, 'assets')))
+  app.use('/queue', express.static(path.join(__dirname, 'queue')))
+
+  // Session storage
+  app.use(session({
+    store: new MemoryStore({ checkPeriod: 86400000 }),
+    secret: randomBytes(32).toString('hex'),
+    resave: false,
+    saveUninitialized: false
+  }))
+
+  const render = (req, res, template, data = {}) => {
+    const baseData = { djsClient: client, path: req.path, user: req.session.user ?? null }
+    res.render(path.join(__dirname, 'templates', template), Object.assign(baseData, data))
+  }
+
+  const checkAuth = (req, res, next) => {
+    if (req.session.user) { return next() }
+    req.session.backURL = req.url
+    res.redirect('/login')
+  }
+
+  // Login endpoint.
+  app.get('/login', (req, res) => {
+    const loginUrl = `https://discordapp.com/api/oauth2/authorize?client_id=${appId}&scope=identify%20guilds&response_type=code&redirect_uri=${encodeURIComponent(`${host}/callback`)}`
+    if (!req.session.user) { return res.redirect(loginUrl) }
+  })
+
+  // Callback endpoint.
+  app.get('/callback', async (req, res) => {
+    if (!req.query.code) { return res.redirect('/') }
+
+    const body = new URLSearchParams({ 'client_id': appId, 'client_secret': clientSecret, 'code': req.query.code, 'grant_type': 'authorization_code', 'redirect_uri': `${host}/callback` })
+    const token = await client.rest.post(Routes.oauth2TokenExchange(), { body: body, headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, auth: false, passThroughBody: true }).catch((e) => { logging.warn('Error while fetching token while authenticating: ' + e) })
+    if (!token?.access_token) { return res.redirect('/login') }
+
+    const user = await client.rest.get(Routes.user(), { headers: { authorization: `${token.token_type} ${token.access_token}` }, auth: false }).catch((e) => { logging.warn('Error while fetching user while authenticating: ' + e) })
+    const guilds = await client.rest.get(Routes.userGuilds(), { headers: { authorization: `${token.token_type} ${token.access_token}` }, auth: false }).catch((e) => { logging.warn('Error while fetching guilds while authenticating: ' + e) })
+    if (!user || !guilds) { return res.redirect('/login') }
+
+    user.guilds = guilds
+    req.session.user = user
+
+    if (req.session.backURL) {
+      res.redirect(req.session.backURL)
+      req.session.backURL = null
+    } else {
+      res.redirect('/')
+    }
+  })
+
+  // Logout endpoint.
+  app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+      res.redirect('/')
+    })
+  })
+
+  // Index endpoint.
+  app.get('/', (req, res) => {
+    render(req, res, 'index.ejs')
+  })
+
+  // Dashboard endpoint.
+  app.get('/dashboard', checkAuth, (req, res) => {
+    render(req, res, 'dashboard.ejs')
+  })
+
+  // Queue endpoint.
+  app.get('/dashboard/:guildId', checkAuth, (req, res) => {
+    const guild = client.guilds.cache.get(req.params.guildId)
+    if (!guild) { return res.redirect('/dashboard') }
+    render(req, res, 'queue.ejs', { guild: guild })
+  })
+
+  // Commands endpoint.
+  app.get('/commands', (req, res) => {
+    const readme = fs.readFileSync('./README.md').toString()
+    const markdown = marked.parse(readme.substring(readme.indexOf('### General'), readme.indexOf('## Installation')))
+    render(req, res, 'commands.ejs', { markdown: markdown })
+  })
+
+  // Admin endpoint.
+  app.get('/admin', checkAuth, (req, res) => {
+    if (req.session.user.id !== adminId) { return res.redirect('/') }
+    render(req, res, 'admin.ejs')
+  })
+
+  // Terms of Service endpoint.
+  app.get('/terms', (req, res) => {
+    render(req, res, 'legal/terms.ejs')
+  })
+
+  // Privacy Policy endpoint.
+  app.get('/privacy', (req, res) => {
+    render(req, res, 'legal/privacy.ejs')
+  })
+
+  // 404
+  app.get('*', (req, res) => {
+    render(req, res, '404.ejs')
+  })
+
+  client.dashboard = app.listen(port, null, null, () => {
+    logging.success(`Dashboard is up and running on port ${port}.`)
+  })
+  client.dashboard.host = host
+
+  // WebSocket
+  const wss = setupWebsocket(client, host)
+  client.dashboard.update = function(player) {
+    client.dashboard.emit('update', player)
+  }
+
+  client.dashboard.shutdown = () => {
+    client.dashboard.close()
+    wss.closeAllConnections()
+  }
+}
---
+++ dashboard/websocket.js
@@ -0,0 +1,191 @@
+import { EmbedBuilder } from 'discord.js'
+import { server as WebsocketServer } from 'websocket'
+import { msToHMS } from '../utilities/utilities.js'
+import { getLanguage } from '../language/locale.js'
+
+const clients = {}
+
+function simplifyPlayer(player) {
+  if (!player) { return {} }
+  return {
+    guild: player.guild,
+    queue: player.queue,
+    current: player.queue.current,
+    paused: player.paused,
+    volume: player.volume,
+    filter: player.filter,
+    position: player.position,
+    timescale: player.timescale,
+    repeatMode: player.queueRepeat ? 'queue' : player.trackRepeat ? 'track' : 'none'
+  }
+}
+
+function send(ws, data) {
+  ws.sendUTF(JSON.stringify(Object.assign({}, data)))
+}
+
+export function setupWebsocket(client, domain) {
+  const wss = new WebsocketServer({ httpServer: client.dashboard })
+
+  // noinspection JSUnresolvedFunction
+  wss.on('request', (request) => {
+    if (request.origin !== domain) { return request.reject() }
+    const ws = request.accept(null, request.origin)
+    let guildId, userId
+
+    ws.on('message', async (message) => {
+      if (message.type !== 'utf8') { return }
+      const data = JSON.parse(message.utf8Data)
+
+      // Add to client manager
+      guildId = data.guildId
+      userId = data.userId
+      clients[data.guildId] = { ...clients[data.guildId], [data.userId]: ws }
+
+      // Fetch guild, user and queue
+      const guild = client.guilds.cache.get(data.guildId)
+      if (!guild) { return ws.close() }
+      const user = await client.users.cache.get(data.userId)
+      if (!user) { return ws.close() }
+      const player = client.lavalink.getPlayer(guild.id)
+      if (!player) { return send(ws, {}) }
+
+      if (data.type === 'request') { return send(ws, simplifyPlayer(player)) }
+
+      if (guild.members.cache.get(user.id)?.voice.channel !== guild.members.me.voice.channel) { return send(ws, { toast: { message: 'You need to be in the same voice channel as the bot to use this command!', type: 'danger' } }) }
+
+      let toast = null
+      switch (data.type) {
+        case 'previous': {
+          if (player.position > 5000) {
+            await player.seek(0)
+            break
+          }
+          try {
+            if (player.previousTracks.length === 0) { return send(ws, { toast: { message: 'You can\'t skip to a previous song!' } }) }
+            const track = player.previousTracks.pop()
+            player.queue.add(track, 0)
+            player.manager.once('trackEnd', (player) => { player.queue.add(player.previousTracks.pop(), 0) })
+            player.stop()
+            toast = { message: 'Skipped to previous song.', type: 'info' }
+          } catch (e) {
+            await player.seek(0)
+          }
+          break
+        }
+        case 'pause': {
+          player.pause(!player.paused)
+          toast = { message: player.paused ? 'Paused.' : 'Resumed.', type: 'info' }
+          break
+        }
+        case 'skip': {
+          player.stop()
+          toast = { message: 'Skipped.', type: 'info' }
+          break
+        }
+        case 'shuffle': {
+          player.queue.shuffle()
+          toast = { message: 'Shuffled the queue.', type: 'info' }
+          break
+        }
+        case 'repeat': {
+          player.trackRepeat ? player.setQueueRepeat(true) : player.queueRepeat ? player.setTrackRepeat(false) : player.setTrackRepeat(true)
+          toast = { message: `Set repeat mode to "${player.queueRepeat ? 'Queue' : player.trackRepeat ? 'Track' : 'None'}"`, type: 'info' }
+          break
+        }
+        case 'volume': {
+          player.setVolume(data.volume)
+          break
+        }
+        case 'filter': {
+          // noinspection JSUnresolvedFunction
+          player.setFilter(data.filter)
+          toast = { message: `Set filter to "${data.filter}"`, type: 'info' }
+          break
+        }
+        case 'play': {
+          if (!data.query) { return }
+          const result = await player.search(data.query, user)
+          if (result.loadType === 'LOAD_FAILED' || result.loadType === 'NO_MATCHES') { return send(ws, { toast: { message: 'There was an error while adding your song/playlist to the queue.', type: 'danger' } }) }
+          const lang = getLanguage(await client.database.getLocale(guild.id)).play
+
+          const embed = new EmbedBuilder()
+            .setAuthor({ name: lang.author, iconURL: user.displayAvatarURL() })
+            .setFooter({ text: 'SuitBot Web Dashboard', iconURL: client.user.displayAvatarURL() })
+
+          if (result.loadType === 'PLAYLIST_LOADED') {
+            player.queue.add(result.tracks)
+            if (player.state !== 'CONNECTED') { await player.connect() }
+            if (!player.playing && !player.paused && player.queue.totalSize === result.tracks.length) { await player.play() }
+
+            // noinspection JSUnresolvedVariable
+            embed
+              .setTitle(result.playlist.name)
+              .setURL(result.playlist.uri)
+              .setThumbnail(result.playlist.thumbnail)
+              .addFields([
+                { name: lang.fields.amount.name, value: lang.fields.amount.value(result.tracks.length), inline: true },
+                { name: lang.fields.author.name, value: result.playlist.author, inline: true },
+                { name: lang.fields.position.name, value: `${player.queue.indexOf(result.tracks[0]) + 1}-${player.queue.indexOf(result.tracks[result.tracks.length - 1]) + 1}`, inline: true }
+              ])
+            toast = { message: `Added playlist "${result.playlist.name}" to the queue.`, type: 'info' }
+          } else {
+            const track = result.tracks[0]
+            player.queue.add(track)
+            if (player.state !== 'CONNECTED') { await player.connect() }
+            if (!player.playing && !player.paused && !player.queue.length) { await player.play() }
+
+            embed
+              .setTitle(track.title)
+              .setURL(track.uri)
+              .setThumbnail(track.thumbnail)
+              .addFields([
+                { name: lang.fields.duration.name, value: track.isStream ? '🔴 Live' : msToHMS(track.duration), inline: true },
+                { name: lang.fields.author.name, value: track.author, inline: true },
+                { name: lang.fields.position.name, value: (player.queue.indexOf(track) + 1).toString(), inline: true }
+              ])
+            toast = { message: `Added track "${track.title}" to the queue.`, type: 'info' }
+          }
+          await client.channels.cache.get(player.textChannel).send({ embeds: [embed] })
+          break
+        }
+        case 'clear': {
+          player.queue.clear()
+          toast = { message: 'Cleared the queue.', type: 'info' }
+          break
+        }
+        case 'remove': {
+          const track = player.queue.remove(data.index - 1)[0]
+          toast = { message: `Removed track #${data.index}: "${track.title}"`, type: 'info' }
+          break
+        }
+        case 'skipto': {
+          const track = player.queue[data.index - 1]
+          player.stop(data.index)
+          toast = { message: `Skipped to #${data.index}: "${track.title}"`, type: 'info' }
+          break
+        }
+      }
+      if (toast) { send(ws, { toast: toast }) }
+      client.dashboard.update(player)
+    })
+
+    ws.on('close', () => {
+      // Remove from client manager
+      if (!guildId || !userId) { return }
+      delete clients[guildId][userId]
+      if (Object.keys(clients[guildId]).length === 0) { delete clients[guildId] }
+    })
+  })
+
+  client.dashboard.on('update', (player) => {
+    if (clients[player.guild]) {
+      for (const user of Object.keys(clients[player.guild])) {
+        const ws = clients[player.guild][user]
+        setTimeout(() => { send(ws, simplifyPlayer(player)) }, 1000)
+      }
+    }
+  })
+
+  return wss
+}
